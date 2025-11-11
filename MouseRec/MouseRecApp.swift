533478ef2230bc9f47c99fd748f212215a84fd2c
stanislas <stanislasperidy@gmail.com>
2025-11-11 17:39:17 +0100

    Initial Commit

---
+++ MouseRec/MouseRecApp.swift
@@ -0,0 +1,17 @@
+//
+//  MouseRecApp.swift
+//  MouseRec
+//
+//  Created by Stanislas Peridy on 11/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct MouseRecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MouseRecTests/MouseRecTests.swift
@@ -0,0 +1,17 @@
+//
+//  MouseRecTests.swift
+//  MouseRecTests
+//
+//  Created by Stanislas Peridy on 11/11/2025.
+//
+
+import Testing
+@testable import MouseRec
+
+struct MouseRecTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
